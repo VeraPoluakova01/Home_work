91899da4f919d7aee3c92b106ab120005810c201
VeraPoluakova01 <polyakova2331@mail.ru>
2023-08-05 19:03:10 +0300

    изменение в 4_2

--- HW_4/task4_2.py
+++ HW_4/task4_2.py
@@ -1,10 +1,9 @@
 """Напишите функцию, принимающую на вход только ключевые параметры и возвращающую словарь, где ключ — значение
 переданного аргумента, а значение — имя аргумента. Если ключ не хешируем, используйте его строковое представление."""
 
-
-def func(**args):
+def func(**kwargs):
     dct = {}
-    for key, value in args.items():
+    for key, value in kwargs.items():
         if value.__hash__ == None:
             value = str(value)
         dct[value] = key
@@ -12,3 +11,6 @@ def func(**args):
 
 
 print(func(a=6, b='World', c=3.1415, d=[0, 10, 3], e=(1, 2, 3)))
+
+
+
